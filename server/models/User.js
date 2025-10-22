5e132bbb089ad4aa2a8602db44f5c96d03188a27
Shivam <shivam34500@gmail.com>
2025-10-22 19:08:56 +0530

    second commit

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: { type: String, required: true },
+    email: { type: String, required: true },
+    full_name: { type: String, required: true },
+    username: { type: String, unique: true },
+    bio: { type: String,default:'Hey there! I am using Aura.' },
+    profile_picture: { type: String, default: '' },
+    cover_picture: { type: String, default: '' },
+    location: { type: String, default: '' },
+    followers: [{ type: String, default: 'User' }],
+    following: [{ type: String, default: 'User' }],
+    connections: [{ type: String, default: 'User' }]
+},{ timestamps: true,minimize:false })
+
+const User = mongoose.model('User',userSchema)
+
+export default User;
